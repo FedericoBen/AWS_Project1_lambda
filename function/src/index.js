9c7b6da86bd67263eae29fdf57a542df47c8d34f
FedericoBen <federicobg95@gmail.com>
2024-09-23 12:39:41 -0500

    Create return in handler function

--- function/src/index.js
+++ function/src/index.js
@@ -1,5 +1,10 @@
-const handler = () =>{
-    console.log('Esta es la fecha actual: ', new Date().toISOString());
-}
+const handler = () => {
+  const newDate = `Esta es la fecha actual: ${new Date().toISOString()}`;
+  console.log(newDate);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newDate),
+  };
+};
 
-export default handler
+export default handler;
