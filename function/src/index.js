8c2ded4c6887cfbb389427d3d50c2f712cfae974
FedericoBen <federicobg95@gmail.com>
2024-09-24 13:08:15 -0500

    Add headers response handler

--- function/src/index.js
+++ function/src/index.js
@@ -2,19 +2,25 @@ const { getUsers } = require("./actions/get");
 const { putUser } = require("./actions/put");
 
 const HANDLER_EVENTS = {
-  "PUT": putUser,
-  "GET": getUsers,
+  PUT: putUser,
+  GET: getUsers,
 };
 
 exports.handler = async (event) => {
   console.log(`Event-Coming : ${JSON.stringify(event)}`);
-  
-  return (
-    HANDLER_EVENTS[event.httpMethod]?.(event) ?? {
+
+  return {
+    ...(HANDLER_EVENTS[event.httpMethod]?.(event) ?? {
       statusCode: 400,
       body: JSON.stringify({
         message: `Operación no soportada. ${event.httpMethod}`,
       }),
-    }
-  );
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "*",
+    },
+  };
 };
